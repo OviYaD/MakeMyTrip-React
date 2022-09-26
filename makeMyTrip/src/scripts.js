03650b193db74fa11ab0ba10984b2f049a7827fa
OviYa <oviya.d@codingmart.com>
2022-09-26 15:04:26 +0530

    removeFiles

--- makeMyTrip/src/scripts.js
+++
@@ -1,9 +0,0 @@
-console.log("java script works");
-
-function oneway() {
-  console.log("oneway");
-}
-function evntreg() {
-  document.getElementById("oneway").addEventListener("onclick", oneway, false);
-}
-window.addEventListener("onload", evntreg, true);
