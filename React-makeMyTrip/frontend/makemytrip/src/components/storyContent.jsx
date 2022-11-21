8694a4cfa25ec2259e3d781b367f73542d96a390
OviYaD <oviya.d@codingmart.com>
2022-11-21 15:14:29 +0530

    added story module

---
+++ React-makeMyTrip/frontend/makemytrip/src/components/storyContent.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export const StoryContent = () => {
+     return <>
+        <div className="appendBottom40">
+                            <div className="dscovrBst">
+                                <p className="dscovrBst__title">
+                                    <font color="#000000"><b>Discover the Best of Luxury</b></font>
+                                </p>
+                                <div className="dscovrBst__cards">
+                                    <div className="dscovrBst__cards--card">
+                                        <div className="dscovrBst__cards--cardBg">
+                                            <img src="//r2imghtlak.mmtcdn.com/r2-mmt-htl-image/htl-imgs/201710052204148585-8615c7d638aa11ebb7b50242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=244:208&amp;crop=244:208" />
+                                        </div>
+                                        <div className="dscovrBst__cards--cardWrapper">
+                                            <img
+                                                src="//r2imghtlak.mmtcdn.com/r2-mmt-htl-image/htl-imgs/201710052204148585-8615c7d638aa11ebb7b50242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=94:94&amp;crop=94:94"
+                                                alt="Proximity to Beach"
+                                                style={{
+                                                    width: "110px",
+                                                    height: "110px",
+                                                    borderRadius: "50%",
+                                                    backgroundImage: "url('data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e %3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'110\' ry=\'110\' stroke=\'%23fff\' stroke-width=\'7\' stroke-dashoffset=\'0\' stroke-dasharray=\'345.57519189487726\' stroke-linecap=\'butt\' /%3e %3c/svg%3e');"}}
+                                            />
+                                            <p className="dscovrBst__cards--title">Proximity to Beach</p>
+                                        </div>
+                                    </div>
+                                    <div className="dscovrBst__cards--card">
+                                        <div className="dscovrBst__cards--cardBg"><img src="//r1imghtlak.mmtcdn.com/cb13d4e2fefc11e99a030242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=244:208&amp;crop=244:208" /></div>
+                                        <div className="dscovrBst__cards--cardWrapper">
+                                            <img
+                                                src="//r1imghtlak.mmtcdn.com/cb13d4e2fefc11e99a030242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=94:94&amp;crop=94:94"
+                                                alt="Delectable Dining"
+                                                style={{
+                                                    width: "110px",
+                                                    height: "110px",
+                                                    borderRadius: "50%",
+                                                    backgroundImage: "url('data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e %3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'110\' ry=\'110\' stroke=\'%23fff\' stroke-width=\'7\' stroke-dashoffset=\'0\' stroke-dasharray=\'345.57519189487726\' stroke-linecap=\'butt\' /%3e %3c/svg%3e');"
+                                                }}
+                                            />
+                                            <p className="dscovrBst__cards--title">Delectable Dining</p>
+                                        </div>
+                                    </div>
+                                    <div className="dscovrBst__cards--card">
+                                        <div className="dscovrBst__cards--cardBg"><img src="//r1imghtlak.mmtcdn.com/b2a6d36efefc11e994570242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=244:208&amp;crop=244:208" /></div>
+                                        <div className="dscovrBst__cards--cardWrapper">
+                                            <img
+                                                src="//r1imghtlak.mmtcdn.com/b2a6d36efefc11e994570242ac110002.jpg?&amp;output-quality=75&amp;output-format=jpg&amp;downsize=94:94&amp;crop=94:94"
+                                                alt="Stunning Outdoor Pool"
+                                                style={{
+                                                    width: "110px",
+                                                    height: "110px",
+                                                    borderRadius: "50%",
+                                                    backgroundImage: "url('data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e %3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'110\' ry=\'110\' stroke=\'%23fff\' stroke-width=\'7\' stroke-dashoffset=\'0\' stroke-dasharray=\'345.57519189487726\' stroke-linecap=\'butt\' /%3e %3c/svg%3e');"
+                                                    }}
+                                            />
+                                            <p className="dscovrBst__cards--title">Stunning Outdoor Pool</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <span></span>
+                        </div>
+     </>
+}
